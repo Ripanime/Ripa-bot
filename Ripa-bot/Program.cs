cd66d49d3712bb7125c7f253e0a6c4a2a1cb16d5
Ripanime <hertekkbaazan@gmail.com>
2023-03-12 20:15:02 +0700

    Add project files.

---
+++ Ripa-bot/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Ripabot.Commands;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Exceptions;
+
+namespace RipaBot
+{
+    internal class Program
+    {
+
+        public static ITelegramBotClient Bot = new TelegramBotClient("6131063078:AAFJdme_MILLu6nfe7u7fUc66LZU-N5c2zs");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text?.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Приветствую");
+                    return;
+                }
+                else if(message.Text?.ToLower() == "прокатиться на бандеромобиле") 
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, $"{message.From.Username} прокатался на бандеромобиле!");
+                    return;
+                }
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Запущен бот " + Bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+            Bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Ripa-bot/RolePlayCommands.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ripabot.Commands
+{
+    internal class RolePlayCommands
+    {
+        var rpcommands = new List<RolePlayCommand>();
+        
+    }
+    internal struct RolePlayCommand 
+    {
+        readonly string _name;
+        readonly string _description;
+        readonly int _commandID;
+        public RolePlayCommand(string name,string description,int CommandID) 
+        {
+            _name = name;
+            _description = description;
+            _commandID = CommandID;
+        }
+    }
+}
